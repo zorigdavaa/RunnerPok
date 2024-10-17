dbf4568504e7f3283c8da2d5cfca9b2fcc1b0a1f
zorigdavaa <zorigoo.nexus@gmail.com>
2024-10-17 18:55:30 +0800

    ss

--- Assets/Scripts/Default/Enemy.cs
+++ Assets/Scripts/Default/Enemy.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ZPackage;
 
 public abstract class Enemy : Character
 {
-
+    public override void TakeDamage(DamageData data)
+    {
+        float multiploer = data.Type.GetEffectiveMultiplier(_Element);
+        float finalDamage = data.damage * multiploer;
+        Health -= (int)finalDamage;
+        healthBar.OnTop();
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
 }
