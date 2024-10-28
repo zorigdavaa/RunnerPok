1e5645a386d1bc33078653fbafee7e7e50b98ced
zorigoo <zorigoo.nexus@gmail.com>
2024-10-28 23:11:16 +0800

    ss

--- Assets/Scripts/Game/RBarriers.cs
+++ Assets/Scripts/Game/RBarriers.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RBarriers : MonoBehaviour
 {
+    List<GameObject> childObjs;
+    public Vector2 minMaxX;
+    List<Vector3> Poses;
     // Start is called before the first frame update
     void Start()
     {
-        
+        minMaxX.x = -5;
+        minMaxX.y = 5;
+        childObjs = transform.GetComponentsInChildren<GameObject>().ToList();
+        float maxDistance = (minMaxX.x - minMaxX.y);
+        float evenDistance = maxDistance / childObjs.Count;
+        for (int i = 0; i < childObjs.Count; i++)
+        {
+            
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
