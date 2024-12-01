57acae11b1f4879923ba68d26b5b760790fd3e65
zorigoo <zorigoo.nexus@gmail.com>
2024-12-02 00:33:29 +0800

    ss

--- Assets/Scripts/Default/UI/Coin.cs
+++ Assets/Scripts/Default/UI/Coin.cs
@@ -8,16 +8,48 @@ public class Coin : MonoBehaviour
     public int Amount;
     float rotateSpeed;
     Transform rotChild;
+    Camera cam;
+    bool shouldRot = true;
     private void Start()
     {
+        cam = Camera.main;
         rotChild = transform.GetChild(0);
         // rotateSpeed = Settings.Instance.CoinRotationSpeed;
         rotateSpeed = 45;
+        GotoPosAndAdd();
     }
     // Update is called once per frame
     void Update()
     {
-        rotChild.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.World);
+        if (shouldRot)
+        {
+            rotChild.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.World);
+        }
+    }
+    public void GotoPosAndAdd()
+    {
+        StartCoroutine(LocalCoroutine());
+        IEnumerator LocalCoroutine()
+        {
+            yield return new WaitForSeconds(2);
+            shouldRot = false;
+            transform.SetParent(null);
+            float t = 0;
+            float time = 0;
+            float duration = 1f;
+            // Vector3 initialPosition = transform.position;
+            while (time < duration)
+            {
+                time += Time.deltaTime;
+                t = time / duration;
+                Vector3 toPos = cam.ScreenToWorldPoint(Z.CanM.Coin.transform.position + Vector3.forward * 10);
+                transform.position = Vector3.Lerp(transform.position, toPos, t);
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * 0.3f, t);
+                yield return null;
+            }
+            GameManager.Instance.Coin++;
+            Destroy(gameObject);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
