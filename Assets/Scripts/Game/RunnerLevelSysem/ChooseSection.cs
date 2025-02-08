86927f445d8b2504c05ba18205a3fd5faa0a3fbf
zorigoo <zorigoo.nexus@gmail.com>
2025-02-09 01:28:03 +0800

    ss

---
+++ Assets/Scripts/Game/RunnerLevelSysem/ChooseSection.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZPackage;
+
+public class ChooseSection : BaseSection
+{
+    public GameObject ChooseTriggerGO;
+    public override void StartSection(Level level)
+    {
+        base.StartSection(level);
+        // float ztoTest = curLevel.lastSpawnedTile.end.transform.position.z;
+        float ztoTest = ChooseTriggerGO.transform.position.z;
+        FunctionTimer.WaitUntilAndCall(curLevel, () => Z.Player.transform.position.z > ztoTest, () => { Skills.Instance.Show3Skills(); });
+    }
+    public override void UpdateSection()
+    {
+        bool isNearEndofLand = curLevel.player.transform.position.z > curLevel.nextSpawnPosition.z - 50;
+        if (isNearEndofLand)
+        {
+            // Tile tileToIns = levelTiles[Random.Range(0, levelTiles.Count)];
+            Tile tileToIns = curLevel.BaseTilePf;
+            curLevel.SpawnTile(tileToIns);
+        }
+    }
+}
