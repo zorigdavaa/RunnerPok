8806300b795925029d63d0d89b0c300ac4102033
Zorigdavaa <zorigoo.nexus@gmail.com>
2025-08-11 16:14:30 +0800

    ss

---
+++ Assets/Scripts/Game/RunnerLevelSysem/SplineInsHelper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplineInsHelper : MonoBehaviour
+{
+    private SplineInstantiate splineInstantiate;
+
+
+    [ContextMenu("Randomize Spline")]
+    public void Randomize()
+    {
+        if (splineInstantiate == null)
+        {
+            splineInstantiate = GetComponent<SplineInstantiate>();
+        }
+        splineInstantiate.Randomize();
+        // splineInstantiate.enabled = false;
+        // splineInstantiate.enabled = true;
+    }
+
+}
