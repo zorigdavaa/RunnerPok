f5fe2c30f641300cf23e036a20dd5a53d2bb2db4
My Mac <mymac@Mys-Mac-mini.local>
2025-08-20 23:52:04 +0800

    ss

--- Assets/CandyKit/Scripts/Core/CKProgressionEvent.cs
+++ Assets/CandyKit/Scripts/Core/CKProgressionEvent.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CandyKitSDK;
+using ZPackage;
+using System;
 
 public class CKProgressionEvent : MonoBehaviour
 {
@@ -9,49 +11,81 @@ public class CKProgressionEvent : MonoBehaviour
     public void Init()
     {
         Debug.LogError("Delete this and uncomment code");
-        // if (!isInited)
+        if (!isInited)
+        {
+            GameManager.Instance.StateChanged += OnGameStateChange;
+            isInited = true;
+        }
+    }
+
+    private void OnGameStateChange(object sender, GameState state)
+    {
+        if (!CandyKit.IsInitialized())
+        {
+            Debug.Log("Candy not ready");
+            return;
+        }
+
+        if (state == GameState.Starting)
+        {
+            CandyKit.NotifyLevelZero();
+        }
+        if (state == GameState.Playing)
+        {
+            CandyKit.NotifyLevelStarted(GameManager.Instance.Level);
+        }
+        else if (state == GameState.LevelCompleted)
+        {
+            CandyKit.NotifyLevelCompleted(GameManager.Instance.Level);
+        }
+        else if (state == GameState.GameOver)
+        {
+            CandyKit.NotifyLevelFailed(GameManager.Instance.Level);
+
+        }
+        // else if (state == GameState.Revive)
         // {
-        //     GameController.OnGameStateChange += OnGameStateChange;
-        //     isInited = true;
+        //     CandyKit.NotifyLevelFailed(GameManager.Instance.Level);
+        //     Debug.Log("Progression Level Revive " + GameController.Level);
         // }
     }
 
-// #if CANDYKIT
-//     void OnGameStateChange(GameState state)
-//     {
-//         if (!CandyKit.IsInitialized())
-//         {
-//             Debug.Log("Candy not ready");
-//             return;
-//         }
 
-//         if (state == GameState.Starting)
-//         {
-//             CandyKit.NotifyLevelZero();
-//             Debug.Log("Progression Level Starting " + GameController.Level);
-//         }
-//         if (state == GameState.Playing)
-//         {
-//             CandyKit.NotifyLevelStarted(GameController.Level);
-//             Debug.Log("Progression Level Start " + GameController.Level);
-//         }
-//         else if (state == GameState.LevelCompleted)
-//         {
-//             CandyKit.NotifyLevelCompleted(GameController.Level);
-//             Debug.Log("Progression Level Complete " + GameController.Level);
-//         }
-//         else if (state == GameState.GameOver)
-//         {
-//             CandyKit.NotifyLevelFailed(GameController.Level);
-//             Debug.Log("Progression Game Over " + GameController.Level);
-//         }
-//         else if (state == GameState.Revive)
-//         {
-//             CandyKit.NotifyLevelFailed(GameController.Level);
-//             Debug.Log("Progression Level Revive " + GameController.Level);
-//         }
-//     }
-// #endif
+    // void OnGameStateChange(GameState state)
+    // {
+    //     if (!CandyKit.IsInitialized())
+    //     {
+    //         Debug.Log("Candy not ready");
+    //         return;
+    //     }
+
+    //     if (state == GameState.Starting)
+    //     {
+    //         CandyKit.NotifyLevelZero();
+    //         Debug.Log("Progression Level Starting " + GameController.Level);
+    //     }
+    //     if (state == GameState.Playing)
+    //     {
+    //         CandyKit.NotifyLevelStarted(GameController.Level);
+    //         Debug.Log("Progression Level Start " + GameController.Level);
+    //     }
+    //     else if (state == GameState.LevelCompleted)
+    //     {
+    //         CandyKit.NotifyLevelCompleted(GameController.Level);
+    //         Debug.Log("Progression Level Complete " + GameController.Level);
+    //     }
+    //     else if (state == GameState.GameOver)
+    //     {
+    //         CandyKit.NotifyLevelFailed(GameController.Level);
+    //         Debug.Log("Progression Game Over " + GameController.Level);
+    //     }
+    //     else if (state == GameState.Revive)
+    //     {
+    //         CandyKit.NotifyLevelFailed(GameController.Level);
+    //         Debug.Log("Progression Level Revive " + GameController.Level);
+    //     }
+    // }
+
 
     // void OnDestroy()
     // {
