8523e83f366c530ab6a18a229c15bd320c48b4c3
Zorigdavaa <zorigoo.nexus@gmail.com>
2025-10-14 17:56:01 +0800

    ss

--- Assets/Scripts/Game/ThreeObs.cs
+++ Assets/Scripts/Game/ThreeObs.cs
@@ -1,16 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ThreeObs : MonoBehaviour
 {
+    public List<Transform> Parents;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (var item in Parents)
+        {
+            int showChild = Random.Range(0, item.childCount);
+            for (int i = 0; i < item.childCount; i++)
+            {
+                item.GetChild(i).gameObject.SetActive(i == showChild);
+            }
+        }
     }
 }
